41e3de61e86cdadd0b4995a4b260a5b9bda18b2f
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2022-10-03 16:47:26 -0700

    Updated README and added comments to Javascript file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Password_Generator_JO
+
+## Generate a Random Password Matching User Criteria
+
+A password generator which includes:
+
+- Prompts askign the user how long they would like the password to be and which types of characters they would like it to include
+- Alerts if the length is outside the range for the generator or no character types have been selected
+- Once the user has made their selections, the password is displayed and can be selected for copying
+- The HTML, CSS, and a bit of Javascript were provided for this challenge and Javascript was used to complete it
+
+## Usage
+
+The following link can be used to view the webpage: 
+https://jessicaob.github.io/Password_Generator_JO/ 
+
+Here is a screenshot of the completed project: 
+
+![Webpage Screenshot](./Assets/PassGenScreenshot.png)
+
+## Credits
+
+Thank you to University of Arizona Bootcamp for providing this challenge.
+
+## License
+
+MIT License
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Necessary variables declared
 var generateBtn = document.querySelector("#generate");
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -7,13 +7,16 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var sCharacter = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "[", "]", ":", ";", "'", "<", ">", "?"];
 var availableCharacters = []
 
-// Write password to the #password input
+// This function was provided, it displays the password to the webpage once it has been generated
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 };
-
+//The following 2 functions were written by me. The 1st one generates the password.
+//Lines 21 and 28-31 include prompts and confirmation windows to collect user input
+//Lines 23-26 and 34-37 are alerts in case of user selections outside the parameters of the generator
+//Line 32 is a variable to guarantee that at least one of each character type that the user has selected will be included
 function generatePassword() {
   var passLength = window.prompt("How many characters would you like your password to contain(between 8-128)?");
 
@@ -32,6 +35,7 @@ function generatePassword() {
     window.alert("You must include at least one character type!")
     return;
   };
+  //Lines 39-54 check the user input and create an array for avaialable characters used to create the password, and pull at least one random character for each character type the user has selected to ensure each type with be in the password
   if (userChoiceLower == true) {
     availableCharacters = availableCharacters.concat(lowerCase);
     guaranteedCharacters.push(getrandom(lowerCase))
@@ -48,7 +52,7 @@ function generatePassword() {
     availableCharacters = availableCharacters.concat(sCharacter);
     guaranteedCharacters.push(getrandom(sCharacter))
   };
-
+// The following for loops pull the correct number of random charaters from the arrays to create the password
   var password = []
   for (var i = 0; i < passLength; i++) {
     var random = Math.floor(Math.random() * availableCharacters.length);
@@ -56,14 +60,15 @@ function generatePassword() {
   };
   for (var i = 0; i < guaranteedCharacters.length; i++) {
     password[i] = guaranteedCharacters[i]
-  }
-  console.log(guaranteedCharacters);
+  };
+  
   return password.join('');
 };
+//This function takes the characters that guarantee each type the user selected and includes them in the password
 function getrandom(array) {
   var randIndex = Math.floor(Math.random() * array.length)
   var randElement = array[randIndex]
   return randElement
-}
-// Add event listener to generate button
+};
+//This listens for the user click
 generateBtn.addEventListener("click", writePassword);
